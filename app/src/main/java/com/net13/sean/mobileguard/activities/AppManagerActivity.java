2abbcd758c05bfb6dc17f75c88b8ebe104e9947e
Sean_Yan <heroyagxiao@126.com>
2017-04-26 12:29:10 +0800

    完成短信,电话拦截功能

---
+++ app/src/main/java/com/net13/sean/mobileguard/activities/AppManagerActivity.java
@@ -0,0 +1,10 @@
+package com.net13.sean.mobileguard.activities;
+
+import android.app.Activity;
+
+/**
+ * Created by SEAN on 2017/4/26.
+ */
+
+public class AppManagerActivity extends Activity {
+}
---
+++ app/src/main/java/com/net13/sean/mobileguard/service/TelSmsBlackService.java
@@ -0,0 +1,10 @@
+package com.net13.sean.mobileguard.service;
+
+import android.app.Service;
+
+/**
+ * Created by SEAN on 2017/4/25.
+ */
+
+public class TelSmsBlackService extends Service {
+}
